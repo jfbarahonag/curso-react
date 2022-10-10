5473deab25a199af71b30c370beadfd214f25957
Juan Felipe Barahona Gonzalez <jbarahona@XM.com.co>
2022-10-10 13:28:45 -0500

    variables were added to be rendered on App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import Header from './components/header'
 import Candle, * as D from './components/candle'
 
 function App() {
+  const description = 'lorem ipsum ';
+  const fragancies = ['frag A', 'frag B', 'frag C'];
   return (
     <div className="App">
       <Header />
       <Candle />
       <D.Fragance />
       <D.Description />
+      Descripcion: {description}
+      <ul>
+        {
+          fragancies.map((fragancy, idx) => 
+            <li className={`${fragancy}`}>
+              { fragancy }
+            </li>
+          )
+        }
+      </ul>
     </div>
   );
 }
